2dd4df2a0dbd7ddfbc268752b08d5f783a417c85
smokemoha <sadisumohammed27@gmail.com>
2025-04-06 14:54:31 +0100

    styled the pages and added login page

--- src/main/java/com/example/emailverification/config/SecurityConfig.java
+++ src/main/java/com/example/emailverification/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.emailverification.config;
 import com.example.emailverification.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,12 +22,20 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(
                         req->req
-                                .requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/register/**", "/login", "/css/**", "/js/**", "/confirm/**").permitAll()
                                 .anyRequest().authenticated()
-                ).formLogin(Customizer.withDefaults())
-                .logout(Customizer.withDefaults())
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
                 .userDetailsService(userService);
         return http.build();
     }
-
 }
--- src/main/java/com/example/emailverification/controller/LoginController.java
+++ src/main/java/com/example/emailverification/controller/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
